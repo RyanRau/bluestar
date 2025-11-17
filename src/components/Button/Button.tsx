602bc07fe83ef18c7eee23361a02c8614dd1dece
Ryan Rau <ryanzrau@gmail.com>
2025-11-16 20:58:26 -0600

    css fix

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -12,22 +12,23 @@ export default function Button({
   return (
     <button   
     className={css`
-      border-radius': '4px',
-      'border': 'none',
-      'font-size': '14px',
-      'font-color': '#FFFFFF',
-      'padding': '8px 20px',
-      'display': 'inline-block',
-      'background-color': '#7DA7D9',
-      ':hover': {
-        'box-shadow':'0 8px 16px 0 rgba(0,0,0,0.6)'
-      }
+        border-radius: 4px;
+        border: none;
+        font-size: 14px;
+        color: #ffffff;
+        padding: 8px 20px;
+        display: inline-block;
+        background-color: #7da7d9;
+
+        &:hover {
+          box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.6);
+        }
       `}
     {...props}>
       <span className={css` 
-          'font-size': '14px',
-          'color': '#FFFFFF',
-        `}> 
+        font-size: 14px;
+        color: #ffffff;
+      `}> 
         {label}
       </span>
     </button>
